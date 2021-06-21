eb60ac049707f178982025869d587afb8819356a
danielLucas <daniel.lucasafj@gmail.com>
2021-06-21 08:09:34 -0300

    Primeiro commit

---
+++ src/components/pages/Home1/Home1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../../App.css'
+import HeroSection from '../../Hero/HeroSecition';
+import Cards from '../../Cards/Cards';
+function Home1 (){
+
+    return(
+        <>
+            <HeroSection></HeroSection>
+            <Cards></Cards>
+        </>
+    )
+}
+
+export default Home1
