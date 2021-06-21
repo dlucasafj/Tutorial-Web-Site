a2479777e31f6855c29c255878d27c3c3d74cbfd
danielLucas <daniel.lucasafj@gmail.com>
2021-06-21 10:25:22 -0300

    Novas rotas implementadas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
 import Home1 from './components/pages/Home1/Home1';
+import Products from './components/pages/Products/Products';
+import Services from './components/pages/Services/Services';
+import SignUp from './components/pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home1}></Route>
+        <Route path='/products' exact component={Products}></Route>
+        <Route path='/services' exact component={Services}></Route>
+        <Route path='/sign-up' exact component={SignUp}></Route>
       </Switch>
     </Router>
 
