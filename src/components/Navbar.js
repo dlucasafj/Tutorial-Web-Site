9088a955f9e339b6bbef968a18e3c8d957b7f00f
danielLucas <daniel.lucasafj@gmail.com>
2021-06-21 10:12:18 -0300

    Rota service acrescentada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
                             </Link>
                         </li>
                          <li className='nav-item'>
-                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                             <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                                  Service
                              </Link>
                          </li>
